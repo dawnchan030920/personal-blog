4ba9ee8f1364a5154279991c81d6fe6a3a4a16d5
dawnchan030920 <dawnchan030920@outlook.com>
2024-02-06 18:02:06 +0800

    [upd] cloud

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,11 +2,10 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: "github",
-    repo: {
-        owner: "dawnchan030920",
-        name: "personal-blog"
-    }
+    kind: "local",
+  },
+  cloud: {
+    project: 'dc392/personal-blog',
   },
   collections: {
     posts: collection({
